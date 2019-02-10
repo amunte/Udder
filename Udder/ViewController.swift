af79f886f6540bad14f305f87fab972fdcdf6349
Albert <albert.munte@gmail.com>
2019-02-09 18:01:27 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Albert Munté on 2/9/19.
+//  Copyright © 2019 Albert Munté. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
